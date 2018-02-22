0fc3ee4adbb62a45442d24288f55d8328dc9ec4e
Tomáš Král <kralutomas@gmail.com>
2018-02-23 00:00:24 +0100

    helper

--- app/helpers/font_awesome/rails/icon_helper.rb
+++ app/helpers/font_awesome/rails/icon_helper.rb
@@ -2,8 +2,18 @@ module FontAwesome
   module Rails
     module IconHelper
 
-      def fa_icon(name, options = {})
-        content_tag(:i, '', class: "fas fa-#{name}")
+      def fa_icon(icon, options = {})
+        classes = ["fas"]
+        classes << "fa-#{icon}"
+        classes += options[:class].split(" ") if options[:class].present?
+        classes << "fa-#{options[:size]}" if options[:size].present?
+        classes << "fa-#{options[:animation]}" if options[:animation].present?
+        classes << options[:animation].split(" ").map{|s| "fa-#{s}"} if options[:animation].present?
+        styles = []
+        icon = content_tag(:i, nil, class: classes.join(" "))
+        if options[:text].present?
+          # TODO
+        end
       end
     end
   end
