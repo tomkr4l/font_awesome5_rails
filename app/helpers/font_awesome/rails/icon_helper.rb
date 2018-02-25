88e16f99585bc4db1a7edc0e8bc2d02a3ad3d389
Tomáš Král <kralutomas@gmail.com>
2018-02-25 23:03:22 +0100

    fix helper method

--- app/helpers/font_awesome/rails/icon_helper.rb
+++ app/helpers/font_awesome/rails/icon_helper.rb
@@ -7,12 +7,17 @@ module FontAwesome
         classes << "fa-#{icon}"
         classes += options[:class].split(" ") if options[:class].present?
         classes << "fa-#{options[:size]}" if options[:size].present?
-        classes << "fa-#{options[:animation]}" if options[:animation].present?
         classes << options[:animation].split(" ").map{|s| "fa-#{s}"} if options[:animation].present?
-        styles = []
-        icon = content_tag(:i, nil, class: classes.join(" "))
+        classes = classes.join(" ")
+        tags = []
         if options[:text].present?
-          # TODO
+          content_tag :span do
+            tags << content_tag(:i, nil, class: classes, style: options[:style], data: options[:data])
+            tags << content_tag(:span, options[:text], style: "padding-left: 5px;#{options[:style]}")
+            tags.join.html_safe
+          end
+        else
+          content_tag(:i, nil, class: classes, style: options[:style], data: options[:data])
         end
       end
     end
