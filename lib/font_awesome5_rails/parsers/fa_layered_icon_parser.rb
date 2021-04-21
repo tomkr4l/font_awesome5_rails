bc06f4917e0f3a2f05ef36875343464d2c85b04c
Mark Oveson <mark.oveson@mx.com>
2021-04-21 08:50:01 -0600

    Add namespacing for Parser classes

--- lib/font_awesome5_rails/parsers/fa_layered_icon_parser.rb
+++ lib/font_awesome5_rails/parsers/fa_layered_icon_parser.rb
@@ -1,29 +1,32 @@
 require_relative "parse_methods"
 
-class FaLayeredIconParser
-  include ParseMethods
+module FontAwesome5Rails
+  module Parsers
+    class FaLayeredIconParser
+      include ParseMethods
 
-  attr_reader :aligned, :style, :size, :title
+      attr_reader :aligned, :style, :size, :title
 
+      def initialize(options)
+        @aligned = options[:aligned].nil? ? true : options[:aligned]
+        @style = options[:style]
+        @size = options[:size]
+        @title = options[:title]
+        @options = options
+      end
 
-  def initialize(options)
-    @aligned = options[:aligned].nil? ? true : options[:aligned]
-    @style = options[:style]
-    @size = options[:size]
-    @title = options[:title]
-    @options = options
-  end
+      def classes
+        @classes ||= parse_classes
+      end
 
-  def classes
-    @classes ||= parse_classes
-  end
+      private
 
-  private
-
-  def parse_classes
-    tmp = ["fa-layers"]
-    tmp << "fa-fw" if @aligned
-    tmp += @options[:class].split(" ") unless @options[:class].nil?
-    tmp.uniq.join(" ").strip
+      def parse_classes
+        tmp = ["fa-layers"]
+        tmp << "fa-fw" if @aligned
+        tmp += @options[:class].split(" ") unless @options[:class].nil?
+        tmp.uniq.join(" ").strip
+      end
+    end
   end
 end
